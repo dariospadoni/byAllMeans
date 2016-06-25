1e506bdc789b18aa4649e5826ada2b20e86fe4d1
Alessandro Annini <alessandro.annini@gmail.com>
2016-06-25 22:06:39 +0200

    added journey

--- www/js/app.js
+++ www/js/app.js
@@ -50,16 +50,6 @@ angular
     }
   })
 
-  .state('tab.login', {
-    url: '/login',
-    views: {
-      'Login': {
-        template: 'templates/Login.html',
-        controller: 'LoginCtrl'
-      }
-    }
-  })
-
   .state('tab.account', {
     url: '/account',
     views: {
---
+++ www/js/controllers/journeyCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular
+
+  .module('byAllMeans')
+
+  .controller('JourneyController', function($scope, $rootScope, $state) {
+
+  });
